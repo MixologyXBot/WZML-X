dfa1ceba2cfd0ea10fbed1db53a2952632791ca0
Silent Demon SD ( MysterySD ) <105407900+SilentDemonSD@users.noreply.github.com>
2026-06-07 22:01:06 +0530

    perf: optimize HyperDL core

--- bot/core/config_manager.py
+++ bot/core/config_manager.py
@@ -82,6 +82,8 @@ class Config:
     MEDIA_GROUP = False
     HYBRID_LEECH = True
     HYPER_THREADS = 0
+    HYPER_PIPELINE = 4
+    HYPER_WRITE_BUFFER = 33554432
     HYDRA_IP = ""
     HYDRA_API_KEY = ""
     NAME_SWAP = ""
